725e73f4db22dc486fffba8699adb09aae5c387c
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-11 19:03:06 +0530

    Day02: Initialize variables and perform basic calculations
    Added variables and basic operations for Day 2 of Python programming.

---
+++ Day02/variables.py
@@ -0,0 +1,50 @@
+#Day 2: 30 Days of python programming
+
+first_name = 'Anvesha'
+last_name = 'Shetty'
+full_name = 'Anvesha Shetty'
+country = 'India'
+city = 'Mangalore'
+age = 20
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+
+print(len(first_name))
+print( (len(first_name)) ==(len(last_name)))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+import math
+radius = float(input('Enter the radius: '))
+circum_of_circle = 2 * math.pi * radius
+area_of_circle = math.pi * radius ** 2
+print('Area: ', area_of_circle)
+print('Circumference: ', circum_of_circle)
+
+First_name = input('Enter your first name: ')
+Last_name = input('Enter your last name: ')
+Country = input('Enter your country: ')
+Age = input('Enter your age: ')
+
+help('keywords')
