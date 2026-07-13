2efa20edd0a9584e33779cd8efa646320e093296
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-13 22:54:52 +0530

    Concatenation functions
    Added functionality to concatenate and print a new string.

---
+++ Day04/concatenate.py
@@ -0,0 +1,10 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+concatenate1 = ['Thirty', 'Days', 'Of', 'Python']
+result1 = ' '.join(concatenate1)
+print(result1)
+
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+concatenate2 = ['Coding', 'For' , 'All']
+result2 = ' '.join(concatenate2)
+print(result2)
+
