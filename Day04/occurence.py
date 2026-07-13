ef45951a009901714936af101ad81a8f6f577437
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 00:22:49 +0530

    Add occurrence.py to find character positions

---
+++ Day04/occurence.py
@@ -0,0 +1,11 @@
+company = 'Coding For All'
+
+#Use index to determine the position of the first occurrence of C in Coding For All.
+print(company.find('C'))
+
+#Use index to determine the position of the first occurrence of F in Coding For All.
+print(company.find('F'))
+
+#Use rfind to determine the position of the last occurrence of l in Coding For All People.
+string = 'Coding For All People'
+print(string.rfind('l'))
