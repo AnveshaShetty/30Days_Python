d33602aa0206924586956bfdf0ae613317353780
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 01:54:32 +0530

    Exercises for string methods and formatting
    Add exercises for string manipulation and formatting in Python.

---
+++ Day04/exercises.py
@@ -0,0 +1,89 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+#  FILE: concatenate.py
+
+
+#Declare a variable named company and assign it to an initial value "Coding For All".
+#Print the variable company using print().
+#Print the length of the company string using len() method and print().
+#Change all the characters to uppercase letters using upper() method.
+#Change all the characters to lowercase letters using lower() method.
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+#Cut(slice) out the first word of Coding For All string.
+#Check if Coding For All string contains a word Coding using the method index, find or other methods.
+#Replace the word coding in the string 'Coding For All' to Python.
+#Change "Python for Everyone" to "Python for All" using the replace method or other methods.
+#Split the string 'Coding For All' using space as the separator (split()) .
+#"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+#What is the character at index 0 in the string Coding For All.
+#What is the last index of the string Coding For All.
+#What character is at index 10 in "Coding For All" string.
+#  FILE: basic_functions.py 
+
+
+#Create an acronym or an abbreviation for the name 'Python For Everyone'.
+#Create an acronym or an abbreviation for the name 'Coding For All'.
+#  FILE: acronym.py
+
+
+#Use index to determine the position of the first occurrence of C in Coding For All.
+#Use index to determine the position of the first occurrence of F in Coding For All.
+#Use rfind to determine the position of the last occurrence of l in Coding For All People.
+#  FILE: occurence.py
+
+
+#Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#  FILE: string_methods.py
+
+
+#Does 'Coding For All' start with a substring Coding?
+#Does 'Coding For All' end with a substring coding?
+#  FILE: substring_functions.py
+
+
+#'   Coding For All      '  , remove the left and right trailing spaces in the given string.
+#  FILE: strip_function.py
+
+
+#Which one of the following variables return True when we use the method isidentifier():
+#    30DaysOfPython
+#    thirty_days_of_python
+#  FILE: identifier_function.py
+
+
+#The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+#  FILE: join_function.py 
+
+
+#Use the new line escape sequence to separate the following sentences
+#I am enjoying this challenge.
+#I just wonder what is next.
+#  FILE: line_sequence.py
+
+
+#Use a tab escape sequence to write the following lines.
+#Name      Age     Country   City
+#Asabeneh  250     Finland   Helsinki
+#  FILE: escape_sequence.py
+
+
+#Use the string formatting method to display the following:
+#radius = 10
+#area = 3.14 * radius ** 2
+#The area of a circle with radius 10 is 314 meters square.
+#  FILE: area.py
+
+
+#Make the following using string formatting methods:
+#8 + 6 = 14
+#8 - 6 = 2
+#8 * 6 = 48
+#8 / 6 = 1.33
+#8 % 6 = 2
+#8 // 6 = 1
+#8 ** 6 = 262144
+#  FILE: string_formatting.py
