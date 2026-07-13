34b1640fa4ccf3cee747eeed1a0c1d95dfa2b940
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 01:19:48 +0530

    Add join function for Python libraries list
    Join a list of Python libraries with a hash and space.

---
+++ Day04/join_function.py
@@ -0,0 +1,4 @@
+#The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+
+py_libraries =  ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print(' #'.join(py_libraries))   # Django #Flask #Bottle #Pyramid #Falcon
