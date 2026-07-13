9aed072895ffa468ddfbc38e03b3616e8258051f
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 00:16:34 +0530

    Create acronym generator for phrases
    Add a script to create acronyms for given phrases.

---
+++ Day04/acronym.py
@@ -0,0 +1,7 @@
+#Create an acronym or an abbreviation for the name 'Python For Everyone'.
+python = 'Python For Everyone'
+print(python[0:19:9]) #Output: Pre
+
+#Create an acronym or an abbreviation for the name 'Coding For All'.
+code = 'Coding For All'
+print(code[0:14:4]) #Output: Cnol
