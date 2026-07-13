19c3a6d95c29e4993443c6162e8b1cd72091eb0b
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 01:11:36 +0530

    Implement identifier function examples
    Add examples of isidentifier() method usage.

---
+++ Day04/identifier_function.py
@@ -0,0 +1,6 @@
+#Which one of the following variables return True when we use the method isidentifier():
+identifier_1 = '30DaysOfPython'
+print(identifier_1.isidentifier()) #False
+
+identifier_2 = 'thirty_days_of_python'
+print(identifier_2.isidentifier()) #True
