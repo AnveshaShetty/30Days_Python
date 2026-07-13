e9a84d71b009eb393ae780f599651d41f463329b
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 00:59:18 +0530

    Add substring functions to check string start and end

---
+++ Day04/substring_functions.py
@@ -0,0 +1,7 @@
+string = 'Coding For All'
+
+#Does 'Coding For All' start with a substring Coding?
+print(string.startswith('Coding'))
+
+#Does 'Coding For All' end with a substring coding?
+print(string.endswith('Coding'))
