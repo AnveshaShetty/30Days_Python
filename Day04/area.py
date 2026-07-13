472ada67aab7ac22946a8876a826b0b9f9e03e9f
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 01:52:55 +0530

    Implement area calculation for a circle
    Calculate the area of a circle and print the result.

---
+++ Day04/area.py
@@ -0,0 +1,11 @@
+#radius = 10
+#area = 3.14 * radius ** 2
+#The area of a circle with radius 10 is 314 meters square.
+
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+result = 'The area of a circle with radius {} is {}'.format(str(radius), str(area))
+print('radius = {}' .format(str(radius)))
+print('area = {} * {} ** 2'.format(str(pi),str(radius)))
+print(result)
