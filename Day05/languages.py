65d3647b8cde2e7339c87b81d87f415f427777ac
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 21:59:40 +0530

    Upadate language.py
    Copy the front_end list to full_stack and insert Python and SQL.

--- Day05/languages.py
+++ Day05/languages.py
@@ -6,3 +6,10 @@ front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 back_end = ['Node','Express', 'MongoDB']
 front_end.extend(back_end)
 print(front_end)    #['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB']
+
+#Copy the joined list and assign it to a variable full_stack, then insert Python and SQL after Redux.
+full_stack = front_end.copy()
+print(full_stack)
+full_stack.insert(5,'Python')
+full_stack.insert(6,'SQL')
+print(full_stack)    #'HTML', 'CSS', 'JS', 'React', 'Redux', 'Python', 'SQL', 'Node', 'Express', 'MongoDB']
