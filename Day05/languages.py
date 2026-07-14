05492e252e8c7b96ded9296b09f0e58475e6a59e
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-14 21:19:26 +0530

    join functions
    Combine front-end and back-end programming languages into a single list.

---
+++ Day05/languages.py
@@ -0,0 +1,8 @@
+#Join the following lists:
+#front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+#back_end = ['Node','Express', 'MongoDB']
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+front_end.extend(back_end)
+print(front_end)    #['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB']
