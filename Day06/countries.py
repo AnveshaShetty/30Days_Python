f28fd6a916dda602a64f28a1dbc70391af8e6972
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-15 21:50:25 +0530

    Implement existence checks for Nordic countries
    Add checks for Nordic countries in a tuple

---
+++ Day06/countries.py
@@ -0,0 +1,7 @@
+#Check if an item exists in tuple:
+#   Check if 'Estonia' is a nordic country
+#   Check if 'Iceland' is a nordic country
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)    #False
+print('Iceland' in nordic_countries)    #True
