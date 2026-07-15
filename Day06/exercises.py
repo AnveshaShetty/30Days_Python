daf3c82e966df351d5111398627401fe3ae9e8be
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-15 21:00:13 +0530

    Exercises for tuple operations
    Added exercises for tuple manipulation and checks.

---
+++ Day06/exercises.py
@@ -0,0 +1,24 @@
+#Level 1
+
+#Create an empty tuple
+#Create a tuple containing names of your sisters and your brothers 
+#Join brothers and sisters tuples and assign it to siblings
+#Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+#Unpack siblings and parents from family_members
+#    FILE: siblings.py
+
+
+#Level 2
+
+#Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+#Change the about food_stuff_tp tuple to a food_stuff_lt list
+#Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+#Slice out the first three items and the last three items from food_stuff_lt list
+#Delete the food_stuff_tp tuple completely
+#    FILE: food_item.py
+
+#Check if an item exists in tuple:
+#   Check if 'Estonia' is a nordic country
+#   Check if 'Iceland' is a nordic country
+#   nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+#    FILE: countries.py
