896d3a1791388c4be8b63a2cda27a2ba3e9d7b2a
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-17 22:54:05 +0530

    Add dog dictionary with attributes and print statement

---
+++ Day08/dogs.py
@@ -0,0 +1,13 @@
+#Create an empty dictionary called dog
+dog = {}
+
+#Add name, color, breed, legs, age to the dog dictionary
+dog = {
+  'name': 'tiger',
+  'color': 'white with black spots',
+  'breed': 'dalmation',
+  'legs': '4'
+}
+
+dog['age'] = '9'
+print(dog)    #{'name': 'tiger', 'color': 'white with black spots', 'breed': 'dalmation', 'legs': '4', 'age': '9'}
