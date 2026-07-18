ef1f376c5335686e41173656dcb8538dc0ba15d2
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-18 20:54:05 +0530

    Add age feedback functionality
    Implement age feedback based on user input for driving eligibility.

---
+++ Day09/exercises.py
@@ -0,0 +1,17 @@
+#Level 1
+
+#Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
+#Enter your age: 30
+#You are old enough to learn to drive.
+#Output:
+#Enter your age: 15
+#You need 3 more years to learn to drive.
+#    FILE: age_feedback.py
+
+#
+#
+#
+#
+#
+#
+#
