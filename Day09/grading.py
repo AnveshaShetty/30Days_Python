9d8cbc6d7385ecb7a63d2e14c5a7b9654172ac05
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-19 01:16:42 +0530

    Add handling grade input

--- Day09/grading.py
+++ Day09/grading.py
@@ -14,5 +14,7 @@ elif grades >= 70 and grades <= 79:
   print("C")
 elif grades >= 60 and grades <= 69:
   print("D")
-else:
+elif grades >= 0 and grades <= 59:
   print("F")
+else:
+  print("Invalid marks")
