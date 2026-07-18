efad5df0a52e46db2fab8fb7b1f4e00a60676c8d
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-18 21:17:44 +0530

    Refactor comments in exercises.py
    Updated comments for age feedback and age gap exercises.

--- Day09/exercises.py
+++ Day09/exercises.py
@@ -1,6 +1,8 @@
 #Level 1
 
-#Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
+#Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: 
+#You are old enough to drive. If below 18 give feedback to wait for the missing amount of years.
+#Output:
 #Enter your age: 30
 #You are old enough to learn to drive.
 #Output:
@@ -8,10 +10,13 @@
 #You need 3 more years to learn to drive.
 #    FILE: age_feedback.py
 
-#
-#
-#
-#
+#Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. 
+#You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. 
+#Output:
+#Enter your age: 30
+#You are 5 years older than me.
+#    FILE: age_gap.py
+
 #
 #
 #
