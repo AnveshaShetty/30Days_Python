724247592e2b5a9df3fa6d68859000e2cdf097b3
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-18 23:13:24 +0530

    Add digit comparison program
    Implement a digit comparison program that takes two user inputs and compares them, providing appropriate output based on their relationship.

---
+++ Day09/digit_comparision.py
@@ -0,0 +1,15 @@
+#Get two numbers from the user using input prompt. 
+#If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b. 
+#Output:
+#Enter number one: 4
+#Enter number two: 3
+#4 is greater than 3
+
+a = int(input("Enter number one: "))
+b = int(input("Enter number two: "))
+if a == b:
+  print(f"{a} is equal to {b}")
+elif a > b:
+  print(f"{a} is greater than {b}")
+else:
+  print(f"{a} is less than {b}")
