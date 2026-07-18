b026ccab632f33413fdba19a16d1edc66b0204c2
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-18 23:48:20 +0530

    Implement fruit existence check and addition
    Add functionality to check and modify fruit list

---
+++ Day09/fruits.py
@@ -0,0 +1,12 @@
+#The following list contains some fruits:
+#fruits = ['banana', 'orange', 'mango', 'lemon']
+#If a fruit doesn't exist in the list add the fruit to the list and print the modified list. 
+#If the fruit exists print('That fruit already exist in the list')
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruit_name = input("Enter a fruit: ")
+if (fruit_name in fruits) == True:
+  print("That fruit already exist")
+else:
+  fruits.append(fruit_name)
+  print(fruits)
