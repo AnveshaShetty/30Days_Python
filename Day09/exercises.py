c003c83aee1a711b64525622a6e175a2b61428f4
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-18 23:15:34 +0530

    Add grading criteria comments in exercises.py
    Added grading criteria comments for student scores.

--- Day09/exercises.py
+++ Day09/exercises.py
@@ -26,12 +26,14 @@
 #    FILE: digit_comparision.py
 
 
-#
-#
-#
-#
-#
-#
-#
-#
+#Level 2
+
+#Write a code which gives grade to students according to theirs scores:
+#90-100, A
+#80-89, B
+#70-79, C
+#60-69, D
+#0-59, F
+#    FILE: grading.py
+
 #
