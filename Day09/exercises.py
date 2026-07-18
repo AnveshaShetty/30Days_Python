1ad8e1eaf0bd43b1324119bd5b2bb74c8f6e423a
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-18 23:43:04 +0530

    Document fruit list handling in exercises.py
    Added comments for fruit list functionality.

--- Day09/exercises.py
+++ Day09/exercises.py
@@ -43,10 +43,13 @@
 #June, July or August, the season is Summer
 #    FILE: months_season.py
 
-#
-#
-#
-#
+#The following list contains some fruits:
+#fruits = ['banana', 'orange', 'mango', 'lemon']
+#If a fruit doesn't exist in the list add the fruit to the list and print the modified list. 
+#If the fruit exists print('That fruit already exist in the list')
+#    FILE: fruits.py
+
+
 #
 #
 #
