5e83b6551980f92bde42f4dc52a60b49c7872ebc
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-19 22:20:06 +0530

    Clean up exercises.py by removing comments
    Removed commented-out sections and unnecessary lines from exercises.py.

--- Day10/exercises.py
+++ Day10/exercises.py
@@ -58,30 +58,14 @@
 #  FILE: sum_even_odd.py
 
 
-#
-#
-#
-#
-#
+#Level 3
 
-#
+#Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
+#Go to the data folder and use the countries_data.py file.
+#    What are the total number of languages in the data
+#    Find the ten most spoken languages from the data
+#    Find the 10 most populated countries in the world
+#  FILE: countries.py
 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+#  FILE: fruits.py
