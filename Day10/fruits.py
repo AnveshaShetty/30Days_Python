0062e787077d2c225511e5f6823032ab0767067b
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-19 23:17:51 +0530

    Implement fruit list reversal in fruits.py
    Added a script to reverse the order of a fruit list using a loop.

---
+++ Day10/fruits.py
@@ -0,0 +1,7 @@
+#This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+fruit = ['banana', 'orange', 'mango', 'lemon']
+reverse_fruit = []
+
+for i in fruit:
+  reverse_fruit.insert(0,i)
+print(reverse_fruit)
