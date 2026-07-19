5514fe969db7c303b55de42c0e6f5f84d62f8308
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-19 22:14:24 +0530

    Add Level 2 exercise comments for sums and evens/odds
    Added comments for Level 2 exercises on summing numbers and separating evens and odds.

--- Day10/exercises.py
+++ Day10/exercises.py
@@ -47,12 +47,17 @@
 #  FILE: even_odd.py
 
 
-#
-#
-#
-#
-#
-#
+#Level 2
+
+#Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+#The sum of all numbers is 5050.
+#  FILE: sum.py
+
+#Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+#The sum of all evens is 2550. And the sum of all odds is 2500.
+#  FILE: sum_even_odd.py
+
+
 #
 #
 #
