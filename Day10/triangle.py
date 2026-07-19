8da9dbfab8eb9c19f211263105d5899b04d61270
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-19 23:26:30 +0530

    Add triangle printing loop in triangle.py
    Implement a loop to print a triangle pattern.

---
+++ Day10/triangle.py
@@ -0,0 +1,11 @@
+#Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+
+for i in range(0,8):
+  print('#' * i)
