a9b674e732b084f4d7bb9b5614fd9805c37dd639
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-20 23:32:25 +0530

    Document functions for exercises in exercises.py
    Added function descriptions and file references for various exercises.

--- Day11/exercises.py
+++ Day11/exercises.py
@@ -3,14 +3,18 @@
 #Declare a function add_two_numbers. It takes two parameters and it returns a sum.
 #    FILE: sum.py
 
-#
-#
-#
-#
-#
-#
-#
-#
+#Area of a circle is calculated as follows: area = π x r x r. Write a function that calculates area_of_circle.
+#    FILE: area_circle.py
+
+#Write a function called add_all_nums which takes arbitrary number of arguments and sums all the arguments. Check if all the list items are number types. If not do give a reasonable feedback.
+#    FILE: number_type.py
+
+#Temperature in °C can be converted to °F using this formula: °F = (°C x 9/5) + 32. Write a function which converts °C to °F, convert_celsius_to-fahrenheit.
+#    FILE: temperature.py
+
+#Write a function called check-season, it takes a month parameter and returns the season: Autumn, Winter, Spring or Summer.
+#    FILE:season.py
+
 #
 #
 #
