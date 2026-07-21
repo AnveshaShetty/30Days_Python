0ac6eea46ec36fb7c069a2eb444239e68bef9423
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-22 01:22:07 +0530

    Implement user ID generation functions
    Added functions to generate random user IDs based on user input for length and quantity.

---
+++ Day12/exercises.py
@@ -0,0 +1,64 @@
+#Level 1
+
+#Write a function which generates a six digit/character random_user_id.
+#  print(random_user_id()) 
+#  '1ee33d'
+
+#Modify the previous task. Declare a function named user_id_gen_by_user. It doesn’t take any parameters but it takes two inputs using input(). One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
+#print(user_id_gen_by_user()) # user input: 5 5
+#output:
+#kcsy2
+#SMFYb
+#bWmeq
+#ZXOYh
+#2Rgxf
+   
+#print(user_id_gen_by_user()) # 16 5
+#1GCSgPLMaBAVQZ26
+#YD7eFwNQKNs7qXaT
+#ycArC5yrRupyG00S
+#UbGxOFI7UXSWAyKN
+#dIV0SSUTgAdKwStr
+#    FILE: generate_random.py 
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
