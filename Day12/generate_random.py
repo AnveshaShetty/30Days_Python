f4402c15cf698ca88b5da8946c11c6c1daabd294
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-22 01:22:00 +0530

    Implement user_id_gen_by_user function
    Added a new function to generate user IDs based on user input for length and quantity.

--- Day12/generate_random.py
+++ Day12/generate_random.py
@@ -1,3 +1,4 @@
+#Write a function which generates a six digit/character random_user_id
 import random 
 import string
 
@@ -7,3 +8,8 @@ def random_user_id():
   return random_id
 
 print(random_user_id())
+
+#Modify the previous task. 
+#Declare a function named user_id_gen_by_user. 
+#It doesn’t take any parameters but it takes two inputs using input(). 
+#One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
