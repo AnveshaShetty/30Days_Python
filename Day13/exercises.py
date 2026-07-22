bec340f76dc01cc148c1c40cd780b21262b70c71
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-22 23:38:27 +0530

    Document list manipulation exercises in exercises.py
    Added comments for various exercises related to list manipulation.

--- Day13/exercises.py
+++ Day13/exercises.py
@@ -2,15 +2,30 @@
 #numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
 #    FILE: filter_negative.py
 
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#Flatten the following list of lists of lists to a one dimensional list :
+#list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#    FILE: flatten_to_one_dim.py
+
+#Using list comprehension create the following list of tuples:
+#[(0, 1, 0, 0, 0, 0, 0),
+#(1, 1, 1, 1, 1, 1, 1),
+#(2, 1, 2, 4, 8, 16, 32),
+#(3, 1, 3, 9, 27, 81, 243),
+#(4, 1, 4, 16, 64, 256, 1024),
+#(5, 1, 5, 25, 125, 625, 3125),
+#(6, 1, 6, 36, 216, 1296, 7776),
+#(7, 1, 7, 49, 343, 2401, 16807),
+#(8, 1, 8, 64, 512, 4096, 32768),
+#(9, 1, 9, 81, 729, 6561, 59049),
+#(10, 1, 10, 100, 1000, 10000, 100000)]
+#    FILE: multiples_tuple.py
+
+#Flatten the following list to a new list:
+#countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+#output:
+#[['FINLAND','FIN', 'HELSINKI'], ['SWEDEN', 'SWE', 'STOCKHOLM'], ['NORWAY', 'NOR', 'OSLO']]
+#    FILE: country_list.py
+
 #
 #
 #
