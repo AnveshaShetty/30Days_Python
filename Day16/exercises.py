7df8f1741f90fbb0ba899631f04a920d63441b9e
Anvesha Shetty <168980482+AnveshaShetty@users.noreply.github.com>
2026-07-26 13:24:16 +0530

    Add comments for datetime exercises in exercises.py

---
+++ Day16/exercises.py
@@ -0,0 +1,5 @@
+#Get the current day, month, year, hour, minute and timestamp from datetime module
+#Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
+#Today is 5 December, 2019. Change this time string to time.
+#Calculate the time difference between now and new year.
+#Calculate the time difference between 1 January 1970 and now.
