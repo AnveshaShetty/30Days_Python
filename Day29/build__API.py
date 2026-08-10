d5f4467879eedf8e8774196d7593440d1fafc93d
An001 <shettyyy06@gmail.com>
2026-08-10 21:19:04 +0530

    Created my own API

--- Day29/build__API.py
+++ Day29/build__API.py
@@ -1,32 +1,76 @@
-from flask import Flask,  Response
+from flask import Flask,  Response, request
 import json
 import os
+import pymongo
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from datetime import datetime
 
 app = Flask(__name__)
 
+MONGODB_URI='mongodb+srv://anvi_python:30python_@30daysofpython.bzhklai.mongodb.net/?appName=30DaysOfPython'
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['thirty_days_of_python']
+
 @app.route('/api/v1.0/students', methods = ['GET'])
-def students ():
-    student_list = [
-        {
-            'name':'Asabeneh',
-            'country':'Finland',
-            'city':'Helsinki',
-            'skills':['HTML', 'CSS','JavaScript','Python']
-        },
-        {
-            'name':'David',
-            'country':'UK',
-            'city':'London',
-            'skills':['Python','MongoDB']
-        },
-        {
-            'name':'John',
-            'country':'Sweden',
-            'city':'Stockholm',
-            'skills':['Java','C#']
-        }
-    ]
-    return Response(json.dumps(student_list), mimetype='application/json')
+def get_student ():
+    students = list(db.students.find({}, {'_id': 0}))
+    return Response(dumps(students), mimetype='application/json')
+
+@app.route('/api/v1.0/students/<id>', methods = ['GET'])
+def single_student (id):
+    student = db.students.find({'_id':ObjectId(id)})
+    return Response(dumps(student), mimetype='application/json')
+
+@app.route('/api/v1.0/students', methods = ['POST'])
+def create_student ():
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.insert_one(student)
+    return Response(dumps({"result": "student created"}), mimetype='application/json')
+
+@app.route('/api/v1.0/students/<id>', methods = ['PUT']) 
+def update_student (id):
+    query = {"_id":ObjectId(id)}
+    name = request.form['name']
+    country = request.form['country']
+    city = request.form['city']
+    skills = request.form['skills'].split(', ')
+    bio = request.form['bio']
+    birthyear = request.form['birthyear']
+    created_at = datetime.now()
+    student = {
+        'name': name,
+        'country': country,
+        'city': city,
+        'birthyear': birthyear,
+        'skills': skills,
+        'bio': bio,
+        'created_at': created_at
+
+    }
+    db.students.update_one(query, {"$set": student})
+    return Response(dumps({"result":"the student has been updated"}), mimetype='application/json')
+
+@app.route('/api/v1.0/students/<id>', methods = ['DELETE'])
+def delete_student (id):
+    db.students.delete_one({"_id":ObjectId(id)})
+    return Response(dumps({"result":"the student has been deleted"}), mimetype='application/json')
 
 
 if __name__ == '__main__':
---
+++ Day29/notes.md
@@ -0,0 +1,2 @@
+http://localhost:5000/api/v1.0/students  run this to see the output in browser
+
